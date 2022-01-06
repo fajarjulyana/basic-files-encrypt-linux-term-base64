e59d130699d51eeb0de9e593b3537c3b6622d52f
Fajar Julyana <90027748+fajarjulyana@users.noreply.github.com>
2022-01-07 02:40:21 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,14 @@
+this file is "fj.png"
+//encryption syntax
+
+base64 fj.png >> fj.encrypt.png // encrypt out as fj.encrypt.png
+
+//look result
+cat fj.encrypt.png 
+
+//decrypt base64
+base64 -d fj.encrypt.png >> out.png // decrypt out as out.png
+
+//copytest
+cat out.png >> test
+base64 -d test >> test.png 
