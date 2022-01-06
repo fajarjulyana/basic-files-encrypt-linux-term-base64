5da678128409f5a95d105aa95d51a1c0d32af721
Fajar Julyana <90027748+fajarjulyana@users.noreply.github.com>
2022-01-07 02:46:11 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,26 @@
-this file is "fj.png"
-//encryption syntax
+#!/bin/bash 
+
+#this file is "fj.png"
+
+#encryption syntax
 
 base64 fj.png >> fj.encrypt.png // encrypt out as fj.encrypt.png
 
-//look result
+#look result
+
 cat fj.encrypt.png 
 
-//decrypt base64
+#decrypt base64
+
 base64 -d fj.encrypt.png >> out.png // decrypt out as out.png
 
-//copytest
+#copytest
+
 cat out.png >> test
+
 base64 -d test >> test.png 
+
+
+
+
+#its'swork
